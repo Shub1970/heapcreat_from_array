f1fb4342820d9ed77ce3eaec68db5deab8b965fd
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2019-04-02 00:42:56 +0530

    Update heapcreat.py

--- heapcreat.py
+++ heapcreat.py
@@ -3,13 +3,15 @@ def heap_short(array,index):
     right=2*index+2
     large=index                             #initali index is the largest number
     if left<=len(array) and right<=len(array):
-        if array[left]>array[index]:
+        if array[left]>array[index] and left<len(array):
             large=left
-        if array[right]>array[large]:
+        if array[right]>array[large] and right<len(array):
             large=right
         if large!=index:
             array[index],array[large]=array[large],array[index]
             heap_short(array,large)
+     else:
+        return
 def heap_create(array):
     last=len(array)//2
     for i in range(last,-1,-1):
